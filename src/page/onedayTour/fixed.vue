e3fc1044c3c6c76b2f8244efb30a46874e865ec0
若愚 <1247620270@qq.com>
2017-12-19 23:20:52 +0800

    完善

---
+++ src/page/onedayTour/fixed.vue
@@ -0,0 +1,258 @@
+<template>
+	<div>
+		<div class="modal-box" v-show="modal"></div>
+		
+	<div class="flexbox-wrap">
+		<div class="flexbox">
+			<ul class="flex-list">
+				<li class="flex-item " @click="handleClassifyClick">
+					<div class="all-classify iconfont">&#xe60b;</div>
+					<div class="all-classifyName">全部分类</div>
+				</li>
+				<li class="flex-item"  @click="handleChooseClick">
+					<div class="all-classify iconfont">&#xe6c4;</div>
+					<div class="all-classifyName">筛选</div>
+				</li>
+				<li class="flex-item"  @click="handleRecommendClick">
+					<div class="all-classify iconfont">&#xe641;</div>
+					<div class="all-classifyName">推荐排序</div>
+				</li>
+			</ul>
+
+			<div class="classify-con" v-show="show">
+				<div class="classify-flexbox">
+					<div class="flexbox-left">
+						<ul class="flexbox-left-list">
+							<li class="flexbox-left-item" v-for="item in allClassify" :key="item.id">
+							{{item.name}}<span>{{item.num}}</span>
+							</li>
+						</ul>
+					</div>
+					<div class="flexbox-right"></div>
+				</div>
+			</div>
+
+			<div class="classify-con filter-con" v-show="Isshow">
+				<div class="classify-flexbox filter-flexbox">
+					<div class="flexbox-left filter-left">
+						<ul class="flexbox-left-list">
+							<li class="flexbox-left-item filter-left-item">
+							 目的地
+							</li>
+							<li class="flexbox-left-item filter-left-item">
+							 出发地
+							</li>
+						</ul>
+					</div>
+					<div class="flexbox-right filter-right">
+						<ul class="filter-right-tolist">
+							<li class="flexbox-left-item filter-right-item">
+							 全部目的地
+							</li>
+							<li class="flexbox-left-item filter-right-item" v-for="item in destination" :key="item.id">
+							 {{item.name}}
+							</li>
+						</ul>
+
+						<ul class="filter-right-fromlist" >
+							<li class="flexbox-left-item filter-right-item">
+							 全部出发地
+							</li>
+							<li class="flexbox-left-item filter-right-item">
+							 北京
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="classify-con sugg-con" v-show="ok">
+				 <div class="flexbox-left ">
+						<ul class="flexbox-left-list sugg-list">
+							<li class="flexbox-left-item sugg-item" v-for="item in suggInfo" :key="item.id">
+								{{item.item}}
+							</li>
+						</ul>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	</div>
+
+</template>
+	
+
+<script>
+  export default {
+    props: ['allClassify', 'destination', 'suggInfo'],
+    data: function () {
+      return {
+        show: false,
+        Isshow: false,
+        ok: false,
+        modal: false
+      }
+    },
+    methods: {
+      handleClassifyClick () {
+        this.show = !this.show
+        this.Isshow = false
+        this.ok = false
+        if (this.show && !this.Isshow && !this.ok) {
+          this.modal = true
+        } else {
+          this.modal = false
+        }
+      },
+
+      handleChooseClick () {
+        this.Isshow = !this.Isshow
+        this.show = false
+        this.ok = false
+        if (this.Isshow && !this.show && !this.ok) {
+          this.modal = true
+        } else {
+          this.modal = false
+        }
+      },
+
+      handleRecommendClick () {
+        this.ok = !this.ok
+        this.show = false
+        this.Isshow = false
+        if (this.ok && !this.show && !this.Isshow) {
+          this.modal = true
+        } else {
+          this.modal = false
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+	
+	.modal-box{
+		width: 100%;
+		height: 100%;
+		position: fixed;
+		left: 0;
+		bottom: 0;
+		background: rgba(0,0,0,.8);
+	}
+	.flexbox-wrap {
+		width: 100%;
+		height: .8rem;
+	    position: fixed;
+		left: 0;
+		bottom: 0;
+	}
+	.flexbox {
+		position: relative;
+		background: rgba(0,0,0,.8);
+	}
+	.flex-list {
+		width: 100%;
+		display: flex;
+		text-align: center;
+	}
+	.flex-item {
+		flex: 1;
+		width: .02rem;
+		padding: .08rem .1rem 0;
+		color: #fff;
+	}
+	.all-classify {
+		height: .32rem;
+		line-height: .32rem;
+	}
+	.all-classifyName {
+		height: .4rem;
+		line-height: .4rem;
+		font-size: .24rem;
+		overflow: hidden;
+
+	}
+	.classify-con {
+		width: 100%;
+		position: absolute;
+		left: 0;
+		bottom: .8rem;
+		background: #fff;
+	}
+	.classify-flexbox {
+		width: 100%;
+		height: 5.28rem;
+		display: flex;
+	}
+	.flexbox-left {
+		height: 5.28rem;
+		background: #f4f5f6;
+		overflow: hidden;
+	}
+	.flexbox-left-list {
+		background: #f4f5f6;
+	}
+	.flexbox-left-item {
+		width: 2.1rem;
+		height: .4rem;
+		overflow: hidden;
+		font-size: .28rem;
+		color: #333;
+		line-height: .4rem;
+		padding: .24rem .1rem;
+		padding-left: .3rem;
+		padding-right: .5rem;
+		background: #fff;
+		position: relative;
+	}
+	.flexbox-left-item:after {
+		position: absolute;
+		content: '';
+		width: 100%;
+		height: 1px;
+		overflow: hidden;
+		left: 0;
+		bottom: 0;
+		border-bottom: 1px solid #eaeaea;
+		transform: scaleY(.5);
+		transform-origin: 0 100%;
+		border-color: #e4e6e8;
+	}
+	.flexbox-left-item span {
+		margin-left: .5rem;
+		float: right;
+	}
+	.flexbox-right {
+		flex:1;
+	}
+	.filter-left-item {
+		width: 1.6rem;
+	}
+	.fiter-right {
+		position: relative;
+	}
+	.filter-right-tolist {
+		overflow: hidden;
+		height: 5.25rem;
+		position: absolute;
+		display: block;
+
+	}
+	.filter-right-item {
+		flex:1;
+	}
+	.filter-right-fromlist {
+		overflow: hidden;
+		height: 2.25rem;
+		position: absolute;
+		display: none;
+	}
+	.sugg-list {
+		text-align: center;
+	}
+	.sugg-item {
+		width: 100%;
+	}
+</style>
---
+++ src/page/onedayTour/nav.vue
@@ -0,0 +1,133 @@
+<template>
+<div class="classify-wrap">
+	<div class="classify-container">
+		<h3 class="classify-title">
+			游玩景点
+			<span class="classify-subtitle">(可多选)</span>
+		</h3>
+		<div class="classify-list">
+			<ul class="classify-list-con"  >
+				<li class="classify-list-item" v-for="item in classify" :key="item.id">{{item.name}}</li>
+			</ul>
+		</div>
+		<div class="classify-right" @click="handleClassifyClick">
+			<transition name="fade"  :duration="10000">
+				<span class="classfity-updown iconfont">&#xe63c;</span>
+			</transition>
+		</div>
+	</div>
+</div>
+</template>
+
+<script>
+	export default {
+	  props: ['classify'],
+	  methods: {
+	    handleClassifyClick () {
+	      alert(123)
+	    }
+	  }
+
+	}
+</script>
+
+<style scoped>
+
+	.classify-wrap {
+		height: .8rem;
+	}
+	.classify-container {
+		position: relative;
+		background: #e5e7e8;
+	}
+	.classify-container::after {
+		content: '';
+	    top: .8rem;
+	    width: 100%;
+	    height: 1px;
+	    position: absolute;
+	    background: #bfc6ca;
+	    transform: scaleY(0.5);
+	    transform-origin: 0 0;
+	}
+	.classify-title {
+		position: relative;
+		display: none;
+		font-size: .28rem;
+		line-height: .8rem;
+		color: #212121;
+		text-indent: .2rem;
+	}
+	.classify-title::after {
+		content: '';
+		width: 100%;
+		height: 1px;
+		position: absolute;
+		top: .8rem;
+		background: #d7dbde;
+		transform: scaleY(0.5);
+		transform-origin: 0 0;
+	}
+	.classify-subtitle {
+		font-size: .24rem;
+	}
+	.classify-list {
+		box-sizing: border-box;
+		position: absolute;
+		left: 0;
+		width: 100%;
+		height: .79rem;
+		max-height: 6rem;
+		background: #e5e7e8;
+		padding: .14rem .12rem;
+		overflow: auto;
+		overflow-y: hidden;
+	}
+	.classify-list-con {
+		width: 15108rem;
+		height: .6rem;
+		overflow: auto;
+		
+	}
+	.classify-list-item {
+		position: relative;
+		float: left;
+		padding: 0 .18rem;
+		background: #fff;
+		font-size: .26rem;
+		color: #212121;
+		min-width: .26rem;
+		line-height: .56rem;
+		border-radius: .04rem;
+		margin: 0 .08rem;
+		text-align: center;
+		
+	}
+	.classify-right {
+		width: .8rem;
+		height: .78rem;
+		position: absolute;
+		top: 0;
+		right: 0;
+		background: #e5e7e8;
+		text-align: center;
+	}
+	.classify-right::after {
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 1px;
+		height: 100%;
+		content: '';
+		overflow: hidden;
+		transform: scaleX(0.5);
+		transform-origin: 0 0;
+		border-left: 1px solid #eaeaea;
+
+	}
+	.classfity-updown {
+		font-size: .28rem;
+		color: #aab2b7;
+		line-height: .78rem;
+	}
+</style>
